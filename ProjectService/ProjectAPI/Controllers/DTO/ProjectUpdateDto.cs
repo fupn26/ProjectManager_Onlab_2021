1084f692a18d1b29d0fc53315f8d5e91686776d2
Fürjes-Benke Péter <fupn26@hotmail.hu>
2021-12-10 19:14:26 +0100

    feat: frontend has been added to docker compose

---
+++ ProjectService/ProjectAPI/Controllers/DTO/ProjectUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectAPI.Controllers.DTO
+{
+    public class ProjectUpdateDto
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public List<string> Members { get; set; }
+    }
+}

cf7fb62bec53659a3f348e39fd9788416c727c2a
Fürjes-Benke Péter <fupn26@hotmail.hu>
2022-02-25 18:23:39 +0100

    feat(#25): minimal email sending logic has been implemented

--- MailService/go.mod
+++ MailService/go.mod
@@ -3,3 +3,8 @@ module MailService
 go 1.17
 
 require github.com/rabbitmq/amqp091-go v0.0.0-20210609115249-03e0554a59cf
+
+require (
+	github.com/sendgrid/rest v2.6.8+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.11.0+incompatible // indirect
+)
--- MailService/main.go
+++ MailService/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"log"
+	"os"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -9,6 +13,26 @@ import (
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func sendEmail() {
+	from := mail.NewEmail(os.Getenv("SENDGRID_SENDER_NAME"), os.Getenv("SENDGRID_SENDER_EMAIL"))
+	subject := "Sending with Twilio SendGrid is Fun"
+	to := mail.NewEmail("", "furjes.peter99@gmail.com")
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	key := os.Getenv("SENDGRID_API_KEY")
+	client := sendgrid.NewSendClient(key)
+	response, err := client.Send(message)
+
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
 	}
 }
 
@@ -47,6 +71,7 @@ func main() {
 	go func() {
 		for d := range msgs {
 			log.Printf("Received a message: %s", d.Body)
+			sendEmail()
 		}
 	}()
 

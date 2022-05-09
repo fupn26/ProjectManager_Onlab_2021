e36e29b6e95800653f2543f00873d2d3664eb8e9
Péter Fürjes-Benke <fupn26@hotmail.hu>
2022-05-09 23:07:26 +0200

    build: directory structure has been changed

---
+++ src/UserService/src/main/java/com/example/controller/interceptor/AuthInterceptor.java
@@ -0,0 +1,38 @@
+package com.example.controller.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Responsible for extracting bearer token, if it is passed as query param, and adding it to the request as attribute.
+ */
+@Component
+@Slf4j
+public class AuthInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String forwardedUri = request.getHeader("X-Forwarded-Uri");
+        if (forwardedUri != null && request.getHeader("Authorization") == null) {
+            log.info("Query params: {}", UriComponentsBuilder.fromUriString(forwardedUri)
+                    .build()
+                    .getQueryParams());
+            log.info("Authorization header: {}", request.getHeader("Authorization"));
+            List<String> accessTokens = UriComponentsBuilder.fromUriString(forwardedUri)
+                    .build()
+                    .getQueryParams()
+                    .get("access_token");
+            if (!accessTokens.isEmpty()) {
+                log.info("Access tokens: {}", accessTokens);
+                request.setAttribute("access_token", accessTokens.get(0));
+            }
+        }
+        return true;
+    }
+}
